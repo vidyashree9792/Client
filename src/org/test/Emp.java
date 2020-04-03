1af87a0d3d84e9211aad16d636aa0a194d9f6314
vidyashree9792 <vshree9792@gmail.com>
2020-04-03 20:03:03 +0530

    My Prpject Code

---
+++ src/org/test/Emp.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Emp {
+ public static void main(String[] args) {
+	int a=10;
+	if (a%2==0) {
+		System.out.println("even");
+	} else {
+        System.out.println("odd");
+	}
+}
+}
